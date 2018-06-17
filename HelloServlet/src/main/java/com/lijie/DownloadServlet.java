7b7b8a5be8c214f7d49e6daa789f17d7d025eb8f
li.jie <li.jie@sand.com.cn>
2018-06-17 17:49:57 +0800

    HttpSession session = request.getSession();

--- HelloServlet/src/main/java/com/lijie/DownloadServlet.java
+++ HelloServlet/src/main/java/com/lijie/DownloadServlet.java
@@ -10,6 +10,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 
 public class DownloadServlet extends HttpServlet {
@@ -28,6 +29,14 @@ public class DownloadServlet extends HttpServlet {
 			throws ServletException, IOException {
 		//response.setContentType("application/jar");
 		response.setContentType("text/html");
+		
+		//HttpSession第一次获取会创建一个session，后面再次访问，获得已经创建的
+		HttpSession session = request.getSession();
+		if(session.isNew()){
+			System.out.println("------第一次创建session");
+		}else{
+			System.out.println("------获取已有的session");
+		}
 		
 		//获取ServletConfig的参数
 		System.out.println("从web.xml中获取ServletConfig的参数,name="+name);
